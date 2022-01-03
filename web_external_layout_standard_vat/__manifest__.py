dfcbbaecee117b7ce245a69e2150372791747094
David Alonso // Solvos <david.alonso@solvos.es>
2022-01-03 19:45:24 +0100

    [ADD] web_external_layout_standard_vat

---
+++ web_external_layout_standard_vat/__manifest__.py
@@ -0,0 +1,19 @@
+# © 2022 Solvos Consultoría Informática (<http://www.solvos.es>)
+# License LGPL-3 - See http://www.gnu.org/licenses/lgpl-3.0.html
+{
+    "name": "Standard External Layout - Move VAT to header",
+    "summary": """
+        Moves VAT information from footer to header for
+        Standard External Layout
+    """,
+    "author": "Solvos",
+    "license": "LGPL-3",
+    "version": "14.0.1.0.0",
+    "category": "Web",
+    "website": "https://github.com/solvosci/slv-web",
+    "depends": ["web"],
+    "data": [
+        "views/web_external_layout_standard.xml",
+    ],
+    "installable": True,
+}
